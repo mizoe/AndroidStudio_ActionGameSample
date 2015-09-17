b62b996fc0c706213a04e53dd92e6830ba7614d0
Tomo Mizoe <mz@july.co.jp>
2015-09-17 14:02:17 +0900

    Step 7-3

--- app/src/main/java/jp/co/july/tokyo/action/GameView.java
+++ app/src/main/java/jp/co/july/tokyo/action/GameView.java
@@ -11,6 +11,8 @@ import android.view.View;
  * Created by it on 2015/09/17.
  */
 public class GameView extends View{
+    private static final int START_GROUND_HEIGHT = 50;
+    private Ground ground;
     private static final Paint PAINT = new Paint();
     private Bitmap droidBitmap;
 
@@ -19,10 +21,18 @@ public class GameView extends View{
     }
 
     public void onDraw(Canvas canvas){
+        int width = canvas.getWidth();
+        int height = canvas.getHeight();
+
         if(droidBitmap == null){
             droidBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.droid);
         }
 
+        if(ground == null){
+            ground = new Ground(0, height - START_GROUND_HEIGHT, width, height);
+        }
+
         canvas.drawBitmap(droidBitmap, 0,0,PAINT);
+        ground.draw(canvas);
     }
 }
