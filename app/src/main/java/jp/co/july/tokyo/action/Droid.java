439fece5475edd505e510ebcfcc861e4d6887754
Tomo Mizoe <mz@july.co.jp>
2015-09-17 15:22:45 +0900

    Step 7-7

--- app/src/main/java/jp/co/july/tokyo/action/Droid.java
+++ app/src/main/java/jp/co/july/tokyo/action/Droid.java
@@ -10,14 +10,28 @@ import android.graphics.Rect;
  * Created by it on 2015/09/17.
  */
 public class Droid {
+    private static final float GRAVITY = 0.8f;
+    private static final float WEIGHT = GRAVITY * 60;
+    private float acc = 0;
+    public void jump(float power){
+        acc = power * WEIGHT;
+    }
+
+    public interface Callback{
+        public int getDistanceFromGround(Droid droid);
+    }
+
+    private final Callback callback;
+
     private static final Paint paint = new Paint();
     private Bitmap bitmap;
 
     final Rect rect;
 
-    public Droid(Bitmap bitmap, int left, int top){
+    public Droid(Bitmap bitmap, int left, int top, Callback callback){
         this.rect = new Rect(left, top, left+bitmap.getWidth(), top+bitmap.getHeight());
         this.bitmap=bitmap;
+        this.callback = callback;
     }
 
     public void draw(Canvas canvas){
@@ -25,6 +39,19 @@ public class Droid {
     }
 
     public void move(){
+        acc -= GRAVITY;
+
+        int distanceFromGround = callback.getDistanceFromGround(this);
+/*        if(distanceFromGround <= 0){
+            return;
+        }
         rect.offset(0, 5);
+        */
+
+        if(acc<0 && acc<-distanceFromGround){
+            acc = -distanceFromGround;
+        }
+
+        rect.offset(0,-Math.round(acc));
     }
 }
--- app/src/main/java/jp/co/july/tokyo/action/GameView.java
+++ app/src/main/java/jp/co/july/tokyo/action/GameView.java
@@ -5,12 +5,38 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.view.MotionEvent;
 import android.view.View;
 
 /**
  * Created by it on 2015/09/17.
  */
-public class GameView extends View{
+public class GameView extends View implements Droid.Callback{
+    private static final int MAX_TOUCH_TIME = 500;
+    private long touchDownStartTime;
+    @Override
+    public boolean onTouchEvent(MotionEvent event){
+        switch(event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                touchDownStartTime = System.currentTimeMillis();
+                return true;
+            case MotionEvent.ACTION_UP:
+                jumpDroid();
+                break;
+        }
+        return super.onTouchEvent(event);
+    }
+    private void jumpDroid(){
+        float time=System.currentTimeMillis() - touchDownStartTime;
+        touchDownStartTime = 0;
+        if(getDistanceFromGround(droid) != 0){
+            return;
+        }
+        if(time>MAX_TOUCH_TIME){
+            time=MAX_TOUCH_TIME;
+        }
+        droid.jump(time / MAX_TOUCH_TIME);
+    }
     private static final int START_GROUND_HEIGHT = 50;
     private Ground ground;
     private Droid droid;
@@ -25,7 +51,7 @@ public class GameView extends View{
 
         if(droid == null){
             Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.droid);
-            droid = new Droid(bitmap, 0,0);
+            droid = new Droid(bitmap, 0,0, this);
         }
 
         if(ground == null){
@@ -37,5 +63,10 @@ public class GameView extends View{
         droid.draw(canvas);
         ground.draw(canvas);
         invalidate();
+    }
+
+    @Override
+    public int getDistanceFromGround(Droid droid) {
+        return ground.rect.top - droid.rect.bottom;
     }
 }
